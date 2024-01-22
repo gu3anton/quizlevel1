8deb3d4e0f12a424fa88c447ca10a29289bc89b6
gu3anton <antonkrs@gmail.com>
2024-01-22 21:45:54 +0700

    quiz level 1 rev

--- main.dart
+++ main.dart
@@ -1,40 +1,65 @@
 //program kalkulator
+/*
+Nama Dike Kartika
+ART 241 - 50039
+22-01-2024
+*/
+
 import 'dart:io';
 
 void main() {
-  print('Kalkulator');
-  print('NIP: ART 241 - 50039');
-  print('Menu :');
-  print('1. Penambahan');
-  print('2. Perkalian');
-  print('3. Hitung Zakat');
-  print('4. Hitung Luas Kotak \n');
-  print('Pilihan ?');
+  print("Mulai kalkulator [y]? ");
+  String? kalkulator = stdin.readLineSync();
 
-  //final pilihan = "";
-  String? pilihan = stdin.readLineSync();
+  switch (kalkulator) {
+    case "y":
+      print('Kalkulator');
+      print('NIP: ART 241 - 50039');
+      print('Menu :');
+      print('1. Penambahan');
+      print('2. Perkalian');
+      print('3. Sebutkan makanan favorite anda?');
+      print('4. Sebutkan rumus menghitung kubus? \n');
+      print('Pilihan ?');
 
-  switch (pilihan) {
-    case "1":
-      print("masukan nilai A :");
-      double a = double.parse(stdin.readLineSync()!);
-      print("masukan nilai B :");
-      double b = double.parse(stdin.readLineSync()!);
-      double hasil = a + b;
-      print('Hasil = $hasil');
-      break;
-    case "2":
-      print("perkalian");
-      break;
-    case "3":
-      print("hitung zakat");
-      break;
-    case "4":
-      print("hitung luas kotak");
+      String? pilihan = stdin.readLineSync();
+
+      switch (pilihan) {
+        case "1":
+          print("masukan nilai A :");
+          double a = double.parse(stdin.readLineSync()!);
+
+          print("masukan nilai B :");
+          double b = double.parse(stdin.readLineSync()!);
+          double hasil = a + b;
+          print('Hasil = $hasil');
+          //print("Kembali ke awal?");
+          //String? kalkulator = stdin.readLineSync();
+          break;
+        case "2":
+          print("masukan nilai A :");
+          double a = double.parse(stdin.readLineSync()!);
+
+          print("masukan nilai B :");
+          double b = double.parse(stdin.readLineSync()!);
+          double hasil = a * b;
+          print('Hasil = $hasil');
+          break;
+        case "3":
+          String? makanan = stdin.readLineSync();
+          print("Jawaban anda $makanan");
+          break;
+        case "4":
+          String? rumus = stdin.readLineSync();
+          print("Jawaban anda $rumus");
+          break;
+        default:
+          print("pilihan menu belum dipilih atau tidak ada");
+          break;
+      }
       break;
     default:
       print("pilihan menu belum dipilih atau tidak ada");
       break;
   }
-  //print(pilihan);
 }
